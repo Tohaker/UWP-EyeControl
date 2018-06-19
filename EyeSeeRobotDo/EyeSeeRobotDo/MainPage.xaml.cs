81d9548482440b64c715bd5f3fb12f587d1e2ce2
Miles Bardon <milesbardon@gmail.com>
2018-06-19 01:44:05 +0100

    Added Robotic Arm icon asset, and began attributing to creators. Started work on UI, creating MainPage and initial code behind.

--- EyeSeeRobotDo/EyeSeeRobotDo/MainPage.xaml.cs
+++ EyeSeeRobotDo/EyeSeeRobotDo/MainPage.xaml.cs
@@ -13,18 +13,37 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace EyeSeeRobotDo
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// The Main Page that launches when the App opens.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            ControlSelectionMenu.IsPaneOpen = !ControlSelectionMenu.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ControlsFrame.CanGoBack) ControlsFrame.GoBack();
+        }
+
+        private void SettingsPanelButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.IsPaneOpen = !SettingsPanel.IsPaneOpen;
+        }
+
+        private void ControlSelectionListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
