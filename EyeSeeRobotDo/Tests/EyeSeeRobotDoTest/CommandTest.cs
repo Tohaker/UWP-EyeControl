3df1a6a74c8c76b02a4c1d314ddb0ae1d7a397aa
milesbardon <milesbardon@gmail.com>
2019-08-11 13:06:21 +0100

    Added project for unit tests.

---
+++ EyeSeeRobotDo/Tests/EyeSeeRobotDoTest/CommandTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EyeSeeRobotDoTest
+{
+    [TestClass]
+    public class CommandTest
+    {
+        [TestMethod]
+        public void ConstructWirelessStatusCheckTest()
+        {
+            Command command = new Command(Communication.Wireless, "hand.dev");
+            String expected = "hand.dev/";
+
+            String actual = command.StatusCheck();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
